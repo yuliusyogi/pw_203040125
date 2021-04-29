224e2969f587967c79d3c1f27f74938e0171be2a
yuliusyogi <yuliusyogi8@gmail.com>
2021-04-29 22:11:20 +0700

    menambahkan database

---
+++ kuliah/pertemuan9/index2.php
@@ -0,0 +1,63 @@
+<?php
+// koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+// ambil data dari tabel mahasiswa / query data mahasiswa
+$result = mysqli_query($conn, "SELECT * FROM mahasiswa")
+
+// ambil data (fetch) mahasiswa object result
+// mysqli_fetch_row() // mengembalikan array numeric
+// mysqli_fetch_assoc() // mengenbalikan array associative
+// mysqli_fecth_array() // mengenbalikan keduanya
+// mysqli_fetch_object()
+
+// while( $mhs = mysqli_fetch_assoc($result) ) {
+//     var_dump($mhs);
+// }
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Halaman Admin</title>
+</head>
+<body>
+    
+<h1>Daftar Mahasiswa</h1>
+
+<table border="1" cellpadding="10" cellspacing="0">
+
+    <tr>
+        <th>No.</th>
+        <th>Aksi</th>
+        <th>Gambar</th>
+        <th>NRP</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Jurusan</th>
+    </tr>
+
+
+    <?php $i = 1; ?>
+    <?php while( $row = mysqli_fetch_assoc($result) ) : ?>
+    <tr>
+        <td><?= $i; ?></td>
+        <td>
+            <a href="">ubah</a> |
+            <a href="">hapus</a>
+        </td>
+        <td><img src="img/<?= $row["gambar"]; ?>" width="50"></td>
+        <td><?= $row["nrp"]; ?></td>
+        <td><?= $row["nama"]; ?></td>
+        <td><?= $row["email"]; ?></td>
+        <td><?= $row["jurusan"]; ?></td>
+    </tr>
+    <?php $i++; ?>
+    <?php endwhile; ?>
+
+
+</table>
+
+</body>
+</html>
