ec8a4347822cf5d08d85fdd054fcf8a78f1682ce
yuliusyogi <yuliusyogi8@gmail.com>
2021-04-29 22:16:10 +0700

    koneksi dan tampil data

---
+++ kuliah/pertemuan10/functions.php
@@ -0,0 +1,26 @@
+<?php
+
+function koneksi()
+{
+  return mysqli_connect('localhost', 'root', '', 'pw_203040125');
+}
+
+function query($query)
+{
+  $conn = koneksi();
+
+  $result = mysqli_query($conn, $query);
+
+  // jika hanya 1 data
+  if (mysqli_num_rows($result) == 1) {
+    return mysqli_fetch_assoc($result);
+  }
+
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return $rows;
+}
