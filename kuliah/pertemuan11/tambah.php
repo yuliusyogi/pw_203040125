7d4fdeba1bbb2cd89521710e94a77f8065874126
yuliusyogi <76626242+yuliusyogi@users.noreply.github.com>
2021-05-06 15:14:30 +0700

    Update tambah.php

--- kuliah/pertemuan11/tambah.php
+++ kuliah/pertemuan11/tambah.php
@@ -7,7 +7,7 @@ if (isset($_POST['tambah'])) {
   if (tambah($_POST) > 0) {
     echo "<script>
             alert('data berhasil ditambahkan');
-            document.location.href = 'latihan3.php';
+            document.location.href = 'index.php';
          </script>";
   } else {
     echo "data gagal ditambahkan!";
