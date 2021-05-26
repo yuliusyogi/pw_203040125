4f2a1b8c565c72395c703ce7e825fc2f4401647d
yuliusyogi <76626242+yuliusyogi@users.noreply.github.com>
2021-05-26 13:11:17 +0700

    Add files via upload

---
+++ tugas2/latihan2c.php
@@ -0,0 +1,51 @@
+<?php
+/*
+    Yulius Yogi Yuwono
+    203040125
+    Jumat 13.00, 05 Februari 2021
+    latihan 2C Praktikum PW
+    https://github.com/yuliusyogi/pw2021_203040125
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 2C Praktikum PW</title>
+
+    <style>
+        .salmon {
+            background-color: salmon;
+            height: 50px;
+            width: 50px;
+            line-height: 50px;
+            text-align: center;
+            margin-bottom: 3px;
+            border-radius: 50px;
+            display: inline-block;
+            border: 2px solid black;
+            font-weight: bold;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    function tumpukanBola($tumpukan)
+    {
+        for ($i = 1; $i <= $tumpukan; $i++) {
+            for ($j = 1; $j <= $i; $j++) {
+                echo "<div class=salmon>$i</div>";
+            }
+            echo "<br>";
+        }
+    }
+    echo tumpukanBola(5);
+    ?>
+
+</body>
+
+</html> 
